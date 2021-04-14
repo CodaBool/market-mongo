1be992a6f38726e4d0fe2bf209d975c51a18dbb8
CodaBool <CodaBool@pm.me>
2021-04-14 12:16:19 -0400

    simplier index with no db call and simple domain

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,59 @@
 import React, { useEffect, useState } from 'react'
 import { useSession } from 'next-auth/client'
 import axios from 'axios'
-import { getUser } from './api/user'
+// import { getUser } from './api/user'
 import { useShoppingCart } from 'use-shopping-cart'
 
-export default function Index({ user }) {
-  const { cartDetails } = useShoppingCart()
+export default function Index(props) {
+  // const { cartDetails } = useShoppingCart()
   const [email, setEmail] = useState('')
-  const [session, loading] = useSession()
+  // const [session, loading] = useSession()
 
-  console.log(cartDetails)
+  // console.log(cartDetails)
 
-  function handleUser() {
-    axios.get('/api/user', { params: { email } })
-      .then(res => {
-        console.log('get =', res.data)
-        if (res.data === 'User not found') {
-          console.log('Could not find the user')
-        }
-      })
-      .catch(err => console.log(err.response.data)) // .response.data
-  }
-  function postUser() {
-    axios.post('/api/user', { email, password: 'beans' })
-      .then(res => console.log('posted =', res.data))
-      .catch(err => console.log(err.response.data)) // .response.data
-  }
+  // function handleUser() {
+  //   axios.get('/api/user', { params: { email } })
+  //     .then(res => {
+  //       console.log('get =', res.data)
+  //       if (res.data === 'User not found') {
+  //         console.log('Could not find the user')
+  //       }
+  //     })
+  //     .catch(err => console.log(err.response.data)) // .response.data
+  // }
+  // function postUser() {
+  //   axios.post('/api/user', { email, password: 'beans' })
+  //     .then(res => console.log('posted =', res.data))
+  //     .catch(err => console.log(err.response.data)) // .response.data
+  // }
 
-  function test() {
-    axios.post('/api/test2')
-      .then(res => console.log(res))
-      .catch(err => console.log(err))
-  }
+  // function test() {
+  //   axios.post('/api/test2')
+  //     .then(res => console.log(res))
+  //     .catch(err => console.log(err))
+  // }
 
   return (
     <div>
-      <button onClick={handleUser} >get user</button>
+      {/* <button onClick={handleUser} >get user</button>
       <button onClick={postUser} >post user</button>
-      <button onClick={test}>test</button>
+      <button onClick={test}>test</button> */}
       <input onChange={e => setEmail(e.target.value)} value={email} />
-      {user?.length ? user.map(user => (
+      {/* {user?.length ? user.map(user => (
         <h1 key={user.email}>{user.email}</h1>
       ))
       : <h1>Could not get users</h1>
-      }
+      } */}
     </div>
   )
 }
 
-export async function getServerSideProps() {
-  let user = []
-  await getUser()
-    .then(resp => user = JSON.parse(JSON.stringify(resp)))
-    .catch(err => console.log('err', err))
-  return {
-    props: { user }
-  }
-}
+// export async function getServerSideProps() {
+//   let user = []
+//   await getUser()
+//     .then(resp => user = JSON.parse(JSON.stringify(resp)))
+//     .catch(err => console.log('err', err))
+//   return {
+//     props: { user }
+//   }
+// }
