840f2bbd0152039237b1267847e94810cf1a3cc1
CodaBool <CodaBool@pm.me>
2021-04-14 02:12:14 -0400

    removed unfinished routes

--- pages/checkout/confirmed.js
+++ pages/checkout/confirmed.js
@@ -1,236 +1,247 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { useSession, signIn } from 'next-auth/client'
-import Button from 'react-bootstrap/Button'
-import Row from 'react-bootstrap/Row'
-import Accordion from 'react-bootstrap/Accordion'
-import Card from 'react-bootstrap/Card'
-import Col from 'react-bootstrap/Col'
-import Form from 'react-bootstrap/Form'
-import InputGroup from 'react-bootstrap/InputGroup'
-import { PencilFill, BoxSeam, Envelope, Receipt, HandIndexThumb, PlusCircle, Plus } from 'react-bootstrap-icons'
-import { useRouter } from 'next/router'
-import { useShoppingCart } from 'use-shopping-cart'
-import CartCards from '../../components/CartCards'
-import useScreen from '../../components/useScreen'
-import { Load, isLoad } from '../../components/Load'
-import { axios, SHIPPING_COST, SHIPPING_EST, getEmail, getId } from '../../constants'
+// import React, { useState, useEffect, useRef } from 'react'
+// import { useSession, signIn } from 'next-auth/client'
+// import Button from 'react-bootstrap/Button'
+// import Row from 'react-bootstrap/Row'
+// import Accordion from 'react-bootstrap/Accordion'
+// import Card from 'react-bootstrap/Card'
+// import Col from 'react-bootstrap/Col'
+// import Form from 'react-bootstrap/Form'
+// import InputGroup from 'react-bootstrap/InputGroup'
+// import { PencilFill, BoxSeam, Envelope, Receipt, HandIndexThumb, PlusCircle, Plus } from 'react-bootstrap-icons'
+// import { useRouter } from 'next/router'
+// import { useShoppingCart } from 'use-shopping-cart'
+// import CartCards from '../../components/CartCards'
+// import useScreen from '../../constants/useScreen'
+// import { Load, isLoad } from '../../components/Load'
+// import { axios, SHIPPING_COST, SHIPPING_EST, getEmail, getId } from '../../constants'
 
-export async function getServerSideProps(context) {
-  const stripe = require('stripe')(process.env.STRIPE_SK)
-  let intent = {}
-  const paymentIntent = await stripe.paymentIntents.retrieve(context.query.payment_intent)
-  if (paymentIntent) {
-    intent = paymentIntent
-  } else {
-    console.log('no intent by id', context.query.payment_intent)
-  }
-  return { props: { intent } }
-}
+// export async function getServerSideProps(context) {
+//   const stripe = require('stripe')(process.env.STRIPE_SK)
+//   let intent = {}
+//   const paymentIntent = await stripe.paymentIntents.retrieve(context.query.payment_intent)
+//   if (paymentIntent) {
+//     intent = paymentIntent
+//   } else {
+//     console.log('no intent by id', context.query.payment_intent)
+//   }
+//   return { props: { intent } }
+// }
   
-export default function Confirmed({ intent }) {
-  const { cartDetails, formattedTotalPrice } = useShoppingCart()
-  const [session, loading] = useSession()
-  const [price, setPrice] = useState(-1)
-  const [error, setError] = useState('')
-  const router = useRouter()
-  const note = useRef(null)
-  var size = useScreen()
-  if (!size) size = 'medium'
-  console.log('intent, on client', intent)
-  if (isLoad(session, loading, true)) return <Load />
+// export default function Confirmed({ intent }) {
+//   const { cartDetails, formattedTotalPrice } = useShoppingCart()
+//   const [session, loading] = useSession()
+//   const [price, setPrice] = useState(-1)
+//   const [error, setError] = useState('')
+//   const router = useRouter()
+//   const note = useRef(null)
+//   var size = useScreen()
+//   if (!size) size = 'medium'
+//   console.log('intent, on client', intent)
+//   if (isLoad(session, loading, true)) return <Load />
 
-  function sendNote() {
-    if (note.current.value.trim()) {
-      console.log('send note:', note.current.value)
-    } else {
-      console.log('note is empty')
-    }
-  }
+//   function sendNote() {
+//     if (note.current.value.trim()) {
+//       console.log('send note:', note.current.value)
+//     } else {
+//       console.log('note is empty')
+//     }
+//   }
 
+//   return (
+//     <>
+//       <h1 className="display-3">Order Complete</h1>
+//       <Row>
+//         <Col className={`mt-5 ${!size.includes('small') && 'border-right pr-5'}`} md={6}>
+//           <>
+//             <BoxSeam className="mr-3 mb-3 d-inline" size={32} />
+//             <h1 className="display-4 d-inline" style={{fontSize: '2.5em'}}>Shipping</h1>
+//             <Card className="mb-3">
+//               <Card.Body>
+//                 {intent.shipping ?
+//                   <>
+//                   <h2 style={{fontWeight: '20'}}>Address</h2>
+//                   <Card className="my-3 p-3">
+//                     <p>{session.user.name}</p>
+//                     <p>{intent.shipping.address.line1}</p>
+//                     {intent.shipping.address.line2 && <p>{intent.shipping.address.line2}</p>}
+//                     <p>{intent.shipping.address.city} {intent.shipping.address.state}, {intent.shipping.address.postal_code}</p>
+
+//                     <InputGroup.Text>
+//                       <Envelope style={{marginRight: '15px'}} size={22}/>{SHIPPING_EST}
+//                     </InputGroup.Text>
+//                     <p className="text-center mt-1 text-muted">USPS Priority Mail</p>
+//                   </Card>
+//                   <h3 style={{fontWeight: '20'}} className="text-muted">Note To Seller</h3>
+//                   <Card>
+//                     <Form.Control 
+//                       as="textarea"
+//                       rows="1"
+//                       placeholder="Optional requests?"
+//                       ref={note}
+//                     />
+//                     <Row>
+//                       <Button className="w-100 m-3" variant="outline-primary" onClick={sendNote}>Send Note</Button>
+//                     </Row>
+//                   </Card>
+//                   </>
+//                   : <Load />
+//                 }
+//               </Card.Body>
+//             </Card>
+//           </>
+//         </Col>
+//         <Col className={`mt-5 ${!size.includes('small') && 'border-left pl-5'}`} md={6}>
+//           <>
+//             <Receipt className="mr-3 mb-3 d-inline" size={32} />
+//             <h1 className="display-4 d-inline" style={{fontSize: '2.5em'}}>Order</h1>
+//             <Card className="p-3">
+//               <>
+//                 <Accordion>
+//                   <Card>
+//                     <Accordion.Toggle as={Card.Header} eventKey="0" className="colorless">
+//                       <h4 className="d-inline" style={{fontWeight: '20'}}>
+//                         <PlusCircle style={{marginRight: '15px', marginBottom: '5px'}} size={20}/>Details
+//                       </h4>
+//                       <p className="float-right" style={{fontSize: '0.8em'}}>
+//                         Click to Show More<HandIndexThumb className="translating" style={{marginLeft: '15px', marginBottom: '4px'}} size={20}/>
+//                       </p>
+//                     </Accordion.Toggle>
+//                     <Accordion.Collapse eventKey="0">
+//                       {/* TODO: store order and retrieve it from stripe */}
+//                       <CartCards isSimple/>
+//                     </Accordion.Collapse>
+//                   </Card>
+//                 </Accordion>
+//                 {/* need to format in oneline when small display */}
+//                 <Card className="p-3">
+//                   <Row className="mt-2">
+//                     <Col>Order ID:</Col>
+//                     <Col className="text-right">{intent.id}</Col>  
+//                   </Row>
+//                   <Row className="mt-2">
+//                     <Col>Receipt Emailed To: </Col>
+//                     <Col className="text-right">{intent.receipt_email}</Col>  
+//                   </Row>
+//                   <Row className="mt-2">
+//                     <Col>Created: </Col>
+//                     <Col className="text-right">{new Date(intent.created * 1000).toLocaleString('en-US')}</Col>  
+//                   </Row>
+//                   <Row className="mt-2">
+//                     <Col>Total</Col>
+//                     <Col className="text-right">$ {(intent.amount_received / 100).toFixed(2)}</Col>  
+//                   </Row>
+//                 </Card>
+//               </>
+//             </Card>
+//           </>
+//         </Col>
+//       </Row>
+//     </>
+//   )
+// }
+
+
+// /* Map Making
+
+//   function handleClick() {
+//     const params = {
+//       apiKey: process.env.NEXT_PUBLIC_HERE_API_KEY,
+//       q: '3126 san leo drive'
+//     }
+//     // params['locationId'] = this.state.locationId;
+//     axios.get('https://geocode.search.hereapi.com/v1/geocode', { params: params })
+//       .then(res => {
+//         console.log(res.data.items[0])
+
+//         console.log('House Number', res.data.items[0].address.houseNumber)
+//         console.log('Street', res.data.items[0].address.street)
+//         console.log('Zipcode', res.data.items[0].address.postalCode)
+//         console.log('City', res.data.items[0].address.city)
+//         console.log('State', res.data.items[0].address.stateCode)
+//       })
+//       .catch(err => {
+//         console.log(err)
+//       })
+
+//     // at=52.5228,13.4124
+//     // q=petrol+station
+//     // limit=1
+//     // in=countryCode%3AUSA
+//     // city=Berlin;country=Germany;street=Friedrichstr;houseNumber=20
+//     // [country, state, county, city, district, street,  houseNumber, postalCode]
+//   }
+
+//   function specificSearch() {
+//     const city = 'Orlando'
+//     const street = 'San Leo Dr'
+//     const houseNumber = '3126'
+//     const postalCode = '32820'
+//     const state = 'Florida'
+
+//     const params = {
+//       apiKey: process.env.NEXT_PUBLIC_HERE_API_KEY,
+//       qq: `city=${city};street=${street};houseNumber=${houseNumber};postalCode=${postalCode};state=${state}`,
+//       in: 'countryCode:USA'
+//     }
+
+//     axios.get('https://geocode.search.hereapi.com/v1/geocode', { params: params })
+//       .then(res => {
+//         console.log(res)
+//       })
+//       .catch(err => {
+//         console.log(err)
+//       })
+//   }
+
+//   function closeToReal() {
+//     const street = 'San Leo Dr'
+//     const houseNumber = '3126'
+
+//     const params = {
+//       apiKey: process.env.NEXT_PUBLIC_HERE_API_KEY,
+//       qq: `street=${street};houseNumber=${houseNumber}`,
+//       in: 'countryCode:USA'
+//     }
+    
+//     axios.get('https://geocode.search.hereapi.com/v1/geocode', { params: params })
+//       .then(res => {
+//         console.log(res)
+//       })
+//       .catch(err => {
+//         console.log(err)
+//       })
+//   }
+
+//   function makeMap() {
+//     const city = 'Orlando'
+//     const street = 'San Leo Dr'
+//     const houseNumber = '3126'
+//     const postalCode = '32820'
+
+//     const params = {
+//       n: houseNumber, // house number
+//       s: street, // street
+//       ci: city, // city
+//       zi: postalCode, // zip
+//     }
+
+//     axios.get('/api/here/getImage', { params: params })
+//       .then(res => {
+//         console.log(res.data)
+//       })
+//       .catch(err => {
+//         console.log(err)
+//       })
+//   }
+// */
+
+
+import React from 'react'
+
+export default function confirmed() {
   return (
-    <>
-      <h1 className="display-3">Order Complete</h1>
-      <Row>
-        <Col className={`mt-5 ${!size.includes('small') && 'border-right pr-5'}`} md={6}>
-          <>
-            <BoxSeam className="mr-3 mb-3 d-inline" size={32} />
-            <h1 className="display-4 d-inline" style={{fontSize: '2.5em'}}>Shipping</h1>
-            <Card className="mb-3">
-              <Card.Body>
-                {intent.shipping ?
-                  <>
-                  <h2 style={{fontWeight: '20'}}>Address</h2>
-                  <Card className="my-3 p-3">
-                    <p>{session.user.name}</p>
-                    <p>{intent.shipping.address.line1}</p>
-                    {intent.shipping.address.line2 && <p>{intent.shipping.address.line2}</p>}
-                    <p>{intent.shipping.address.city} {intent.shipping.address.state}, {intent.shipping.address.postal_code}</p>
-
-                    <InputGroup.Text>
-                      <Envelope style={{marginRight: '15px'}} size={22}/>{SHIPPING_EST}
-                    </InputGroup.Text>
-                    <p className="text-center mt-1 text-muted">USPS Priority Mail</p>
-                  </Card>
-                  <h3 style={{fontWeight: '20'}} className="text-muted">Note To Seller</h3>
-                  <Card>
-                    <Form.Control 
-                      as="textarea"
-                      rows="1"
-                      placeholder="Optional requests?"
-                      ref={note}
-                    />
-                    <Row>
-                      <Button className="w-100 m-3" variant="outline-primary" onClick={sendNote}>Send Note</Button>
-                    </Row>
-                  </Card>
-                  </>
-                  : <Load />
-                }
-              </Card.Body>
-            </Card>
-          </>
-        </Col>
-        <Col className={`mt-5 ${!size.includes('small') && 'border-left pl-5'}`} md={6}>
-          <>
-            <Receipt className="mr-3 mb-3 d-inline" size={32} />
-            <h1 className="display-4 d-inline" style={{fontSize: '2.5em'}}>Order</h1>
-            <Card className="p-3">
-              <>
-                <Accordion>
-                  <Card>
-                    <Accordion.Toggle as={Card.Header} eventKey="0" className="colorless">
-                      <h4 className="d-inline" style={{fontWeight: '20'}}>
-                        <PlusCircle style={{marginRight: '15px', marginBottom: '5px'}} size={20}/>Details
-                      </h4>
-                      <p className="float-right" style={{fontSize: '0.8em'}}>
-                        Click to Show More<HandIndexThumb className="translating" style={{marginLeft: '15px', marginBottom: '4px'}} size={20}/>
-                      </p>
-                    </Accordion.Toggle>
-                    <Accordion.Collapse eventKey="0">
-                      {/* TODO: store order and retrieve it from stripe */}
-                      <CartCards isSimple/>
-                    </Accordion.Collapse>
-                  </Card>
-                </Accordion>
-                {/* need to format in oneline when small display */}
-                <Card className="p-3">
-                  <Row className="mt-2">
-                    <Col>Order ID:</Col>
-                    <Col className="text-right">{intent.id}</Col>  
-                  </Row>
-                  <Row className="mt-2">
-                    <Col>Receipt Emailed To: </Col>
-                    <Col className="text-right">{intent.receipt_email}</Col>  
-                  </Row>
-                  <Row className="mt-2">
-                    <Col>Created: </Col>
-                    <Col className="text-right">{new Date(intent.created * 1000).toLocaleString('en-US')}</Col>  
-                  </Row>
-                  <Row className="mt-2">
-                    <Col>Total</Col>
-                    <Col className="text-right">$ {(intent.amount_received / 100).toFixed(2)}</Col>  
-                  </Row>
-                </Card>
-              </>
-            </Card>
-          </>
-        </Col>
-      </Row>
-    </>
+    <div>
+      Still working on this route
+    </div>
   )
 }
-
-
-/* Map Making
-
-  function handleClick() {
-    const params = {
-      apiKey: process.env.NEXT_PUBLIC_HERE_API_KEY,
-      q: '3126 san leo drive'
-    }
-    // params['locationId'] = this.state.locationId;
-    axios.get('https://geocode.search.hereapi.com/v1/geocode', { params: params })
-      .then(res => {
-        console.log(res.data.items[0])
-
-        console.log('House Number', res.data.items[0].address.houseNumber)
-        console.log('Street', res.data.items[0].address.street)
-        console.log('Zipcode', res.data.items[0].address.postalCode)
-        console.log('City', res.data.items[0].address.city)
-        console.log('State', res.data.items[0].address.stateCode)
-      })
-      .catch(err => {
-        console.log(err)
-      })
-
-    // at=52.5228,13.4124
-    // q=petrol+station
-    // limit=1
-    // in=countryCode%3AUSA
-    // city=Berlin;country=Germany;street=Friedrichstr;houseNumber=20
-    // [country, state, county, city, district, street,  houseNumber, postalCode]
-  }
-
-  function specificSearch() {
-    const city = 'Orlando'
-    const street = 'San Leo Dr'
-    const houseNumber = '3126'
-    const postalCode = '32820'
-    const state = 'Florida'
-
-    const params = {
-      apiKey: process.env.NEXT_PUBLIC_HERE_API_KEY,
-      qq: `city=${city};street=${street};houseNumber=${houseNumber};postalCode=${postalCode};state=${state}`,
-      in: 'countryCode:USA'
-    }
-
-    axios.get('https://geocode.search.hereapi.com/v1/geocode', { params: params })
-      .then(res => {
-        console.log(res)
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }
-
-  function closeToReal() {
-    const street = 'San Leo Dr'
-    const houseNumber = '3126'
-
-    const params = {
-      apiKey: process.env.NEXT_PUBLIC_HERE_API_KEY,
-      qq: `street=${street};houseNumber=${houseNumber}`,
-      in: 'countryCode:USA'
-    }
-    
-    axios.get('https://geocode.search.hereapi.com/v1/geocode', { params: params })
-      .then(res => {
-        console.log(res)
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }
-
-  function makeMap() {
-    const city = 'Orlando'
-    const street = 'San Leo Dr'
-    const houseNumber = '3126'
-    const postalCode = '32820'
-
-    const params = {
-      n: houseNumber, // house number
-      s: street, // street
-      ci: city, // city
-      zi: postalCode, // zip
-    }
-
-    axios.get('/api/here/getImage', { params: params })
-      .then(res => {
-        console.log(res.data)
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }
-*/
