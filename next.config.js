279c4b6459c09137cac39de6204a818da7b0f89c
Coda Bool <codabool@pm.me>
2021-04-15 19:52:59 -0400

    env export

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  target: 'serverless',
+  env: {
+    NEXT_PUBLIC_STRIPE_PK: process.env.NEXT_PUBLIC_STRIPE_PK,
+    NEXT_PUBLIC_STAGE: process.env.NEXT_PUBLIC_STAGE,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: 'https://d3dnulbm6gi6yu.cloudfront.net',
+    MONGODB_URI: process.env.MONGODB_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    STRIPE_SK: process.env.STRIPE_SK
+  }
+};
