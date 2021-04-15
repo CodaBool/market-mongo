74c3d99b454621c50a5712cf1316f22cb5a6ff66
Coda Bool <codabool@pm.me>
2021-04-15 18:55:28 -0400

    does cf change this

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     NEXT_PUBLIC_STRIPE_PK: process.env.NEXT_PUBLIC_STRIPE_PK,
     NEXT_PUBLIC_STAGE: process.env.NEXT_PUBLIC_STAGE,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_URL: 'd33isfp79lbv81.cloudfront.net',
     MONGODB_URI: process.env.MONGODB_URI,
     JWT_SECRET: process.env.JWT_SECRET,
     STRIPE_SK: process.env.STRIPE_SK
