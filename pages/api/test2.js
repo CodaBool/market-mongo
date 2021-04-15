c155e06859ab18f0abd73d631d2d19512c43a48c
Coda Bool <codabool@pm.me>
2021-04-14 20:18:49 -0400

    sign jwt key added

--- pages/api/test2.js
+++ pages/api/test2.js
@@ -5,6 +5,9 @@ export default async (req, res) => {
     console.log('NEXT_PUBLIC_STAGE =', process.env.NEXT_PUBLIC_STAGE)
     console.log('NEXTAUTH_URL =', process.env.NEXTAUTH_URL)
 
+
+    // TEST2 has the nextauth_url env, meaning 
+
     const envVars = { uri: process.env.MONGODB_URI, stage: process.env.NEXT_PUBLIC_STAGE, auth: process.env.NEXTAUTH_URL}
 
     res.status(200).json(envVars)
