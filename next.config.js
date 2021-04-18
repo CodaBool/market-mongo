dee3ec23047767fc05e93389c9ecf31d7ee4dfcd
Coda Bool <codabool@pm.me>
2021-04-18 16:38:19 -0400

    runtime env added

--- next.config.js
+++ next.config.js
@@ -4,8 +4,11 @@ module.exports = {
     NEXT_PUBLIC_STRIPE_PK: process.env.NEXT_PUBLIC_STRIPE_PK,
     NEXT_PUBLIC_STAGE: process.env.NEXT_PUBLIC_STAGE,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     MONGODB_URI: process.env.MONGODB_URI,
     STRIPE_SK: process.env.STRIPE_SK
-  }
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    NEXTAUTH_URL: 'https://d1m7a4gmurbqh2.cloudfront.net',
+  },
 };
