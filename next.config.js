67bf40415b764e5fa1ebb1b26a8854fd8e0093c9
Coda Bool <codabool@pm.me>
2021-04-18 17:05:35 -0400

    set internal url instead

--- next.config.js
+++ next.config.js
@@ -4,13 +4,13 @@ module.exports = {
     NEXT_PUBLIC_STRIPE_PK: process.env.NEXT_PUBLIC_STRIPE_PK,
     NEXT_PUBLIC_STAGE: process.env.NEXT_PUBLIC_STAGE,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     MONGODB_URI: process.env.MONGODB_URI,
+    NEXTAUTH_URL_INTERNAL: process.env.NEXTAUTH_URL,
     STRIPE_SK: process.env.STRIPE_SK,
     RANDO: 'this rando env var exists'
   },
   publicRuntimeConfig: {
     // Will be available on both server and client
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_URL_INTERNAL: process.env.NEXTAUTH_URL
   },
 };
