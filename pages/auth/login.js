bf54bc8e6e52199f4e02312577c5f0c1146dd10a
Coda Bool <codabool@pm.me>
2021-04-19 01:36:45 -0400

    enough to test serverside

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -112,10 +112,12 @@ export default function Login({ csrf }) {
 
 export async function getServerSideProps(context) {
   // const csrf = await csrfToken(context)
-  let csrf = undefined
+  console.log('ENV =', process.env)
+  let csrf = {}
   await getCsrfToken(context)
     .then(res => {csrf= res; console.log('csrf=', res)})
     .catch(err => console.log(err))
+  if (!csrf) csrf = 'didn\'t work'
   return {
     props: { csrf  }
   }
