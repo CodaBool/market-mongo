5be1759a3f4bec65fa9483b7899107b6e04fdf5e
Coda Bool <codabool@pm.me>
2021-05-08 00:49:12 -0400

    added env

--- webhook/index.js
+++ webhook/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (req, res) => {
-  res.send('create an .env file if needed')
+  res.send('create a new .env file if needed')
 })
 
 app.listen(3001, () =>
